719d11cadda7d869fb2d6d56a66250098167ef37
Nowhereman8 <61270422+Nowhereman8@users.noreply.github.com>
2020-04-13 16:47:48 +0300

    feat: dorabotka blocks

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -4,11 +4,13 @@ import kotlin.reflect.KClass
 
 class Blocks {   
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
+        var b = blockB
+        var a = blockA
         return when(b) {
-             String :: class -> a.filterIsInstance<String>().joinToString("")
-             Int :: class -> a.filterIsInstance<Int>().sum()
-             LocalDate :: class -> a.filterIsInstance<LocalDate>().max()!!.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
-             else -> ""
+            String :: class -> a.filterIsInstance<String>().joinToString("")
+            Int :: class -> a.filterIsInstance<Int>().sum()
+            LocalDate :: class -> a.filterIsInstance<LocalDate>().max()!!.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            else -> ""
         }
     }
 }

8efbaeec9d2de258c5ec2beab242e3f9e5d2b36e
Nowhereman8 <61270422+Nowhereman8@users.noreply.github.com>
2020-04-13 11:45:27 +0300

    feat: block done

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -2,10 +2,13 @@ package subtask5
 
 import kotlin.reflect.KClass
 
-class Blocks {
-
-    // TODO: Complete the following function
+class Blocks {   
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        return when(b) {
+             String :: class -> a.filterIsInstance<String>().joinToString("")
+             Int :: class -> a.filterIsInstance<Int>().sum()
+             LocalDate :: class -> a.filterIsInstance<LocalDate>().max()!!.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+             else -> ""
+        }
     }
 }
